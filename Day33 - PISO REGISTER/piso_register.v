dcc7adda34e0b75806321716d41fd53b0d1cd8e0
ManojD-22 <manojdasm200@gmail.com>
2025-02-21 23:33:25 +0530

    parallel input serial output

--- Day33 - PISO REGISTER/piso_register.v
+++ Day33 - PISO REGISTER/piso_register.v
@@ -2,16 +2,27 @@ module piso_register(
 
     input clk,
     input reset,
+    input load,
     input [3:0] d,
     output reg q
 );
 
+reh [3:0] shift_reg;
+
+
 always@(posedge clk or posedge reset)begin
     
     if(reset)begin
-
-    end
+        shift_reg <= 4'b0000;
+    end else if(load) 
+                begin
+                    shift_reg <= d; 
+                end 
+            else
+                shift_reg <= {shift_reg{2:0}, 1'b0};
     
 end
+
+assign q = shift_reg[3];
 
 endmodule
