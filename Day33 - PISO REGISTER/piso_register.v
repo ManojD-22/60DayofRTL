621dc53424b59975dbf13433fb21b0e3df4d671a
ManojD-22 <manojdasm200@gmail.com>
2025-02-26 22:54:19 +0530

    parallel input serial output register

--- Day33 - PISO REGISTER/piso_register.v
+++ Day33 - PISO REGISTER/piso_register.v
@@ -1,3 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.02.2025 22:40:22
+// Design Name: 
+// Module Name: piso_register
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module piso_register(
 
     input clk,
@@ -10,19 +32,20 @@ module piso_register(
 reg [3:0] shift_reg;
 
 
-always@(posedge clk or posedge reset)begin
-    
-    if(reset)begin
-        shift_reg <= 4'b0000;
-    end else if(load) 
+always@(posedge clk or posedge reset)
+    begin
+        if(reset)
+            begin
+                shift_reg <= 4'b0000;
+                q <= 1'b0;
+            end 
+        else if(load) 
                 begin
                     shift_reg <= d; 
                 end 
-            else
-                shift_reg <= {shift_reg{2:0}, 1'b0};
-    
-end
-
-assign q = shift_reg[3];
-
+        else begin
+                    shift_reg <= {shift_reg[2:0], 1'b0};
+                end
+              q <= shift_reg[3];
+       end
 endmodule
--- Day33 - PISO REGISTER/tb_piso_register.v
+++ Day33 - PISO REGISTER/tb_piso_register.v
@@ -1,9 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.02.2025 22:48:33
+// Design Name: 
+// Module Name: tb_piso_register
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module tb_piso_register();
-    reg  clk,
-    reg reset,
-    reg load,
-    reg [3:0] d,
-    wire q
+    reg  clk;
+    reg reset;
+    reg load;
+    reg [3:0] d;
+    wire q;
 
 
    piso_register DUT (
@@ -33,6 +55,11 @@ initial begin
     #45 reset = 1;
     #100 $finish;
 end
+ 
+ 
+ initial begin
+    $monitor("%t | clk = %b | reset : %b | load :%b | d : %b | q :%b ", $time, clk, reset, load, d ,q);
+ end
  
 
 
