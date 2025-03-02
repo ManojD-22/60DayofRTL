de9201dae249ac80a6723e84392c47a6c1221a21
ManojD-22 <manojdasm200@gmail.com>
2025-03-02 23:51:29 +0530

    single port ram

--- Day34 - SINGLE PORT RAM/single_port_ram.v
+++ Day34 - SINGLE PORT RAM/single_port_ram.v
@@ -1,15 +1,25 @@
-module single_port_ram(
-    input data,
-    input addr,
+module single_port_ram #(parameter DATA_WIDTH = 8 , ADD_WIDTH = 4)(
     input clk,
     input reset,
-    output wdata
+    input we,
+    input [DATA_WIDTH-1:0] data,
+    input [ADD_WIDTH-1:0] addr,
+    output reg wdata
 );
+
+reg [DATA_WIDTH-1:0] mem [ADD_WIDTH-1:0];
 
 always@(posedge clk or negedge reset)begin
     
     if(!reset)begin
         wdata <= 1'b0;
-    end else
+    end else begin
+        if(we) begin
+            mem[addr] <= data;   // write operation 
+
+            wdata <= mem[addr];  // read operation
+        end
+    end
+
         
 end
